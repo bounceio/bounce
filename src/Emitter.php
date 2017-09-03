42e14dcd2ae02eaca5eca8046a9eb95f86757621
Barney Hanlon <barney@shrikeh.net>
2017-09-03 11:46:01 +0100

    Add basic interface skeleton

--- src/Emitter.php
+++ src/Emitter.php
@@ -4,12 +4,14 @@ namespace Bounce\Bounce;
 
 use EventIO\InterOp\EmitterInterface;
 use EventIO\InterOp\EventInterface;
+use EventIO\InterOp\ListenerAcceptorInterface;
+use EventIO\InterOp\ListenerInterface;
 
 /**
  * Class Emitter
  * @package Bounce\Bounce
  */
-class Emitter implements EmitterInterface
+class Emitter implements EmitterInterface, ListenerAcceptorInterface
 {
     /**
      * @param array ...$events The event triggered
@@ -36,5 +38,20 @@ class Emitter implements EmitterInterface
     public function emitName($event)
     {
         // TODO: Implement emitName() method.
+    }
+
+    /**
+     * @param string $eventName The name of the event to listen for
+     * @param callable|ListenerInterface $listener A listener or callable
+     * @param int $priority Used to prioritise listeners for the same event
+     * @return mixed
+     */
+    public function addListener(
+        $eventName,
+        $listener,
+        $priority = self::PRIORITY_NORMAL
+    )
+    {
+        // TODO: Implement addListener() method.
     }
 }
--- tests/spec/EmitterSpec.php
+++ tests/spec/EmitterSpec.php
@@ -4,6 +4,7 @@ namespace spec\Bounce\Bounce;
 
 use Bounce\Bounce\Emitter;
 use EventIO\InterOp\EmitterInterface;
+use EventIO\InterOp\ListenerAcceptorInterface;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
 
@@ -12,5 +13,10 @@ class EmitterSpec extends ObjectBehavior
     function it_is_an_event_emitter()
     {
         $this->shouldHaveType(EmitterInterface::class);
+    }
+
+    function it_is_a_listener_acceptor()
+    {
+        $this->shouldHaveType(ListenerAcceptorInterface::class);
     }
 }
